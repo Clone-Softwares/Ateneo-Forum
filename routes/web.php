919d5767a23cd337452d5b10877ef0a6c0f39baa
Lol <133163245+Tantalost@users.noreply.github.com>
2025-09-25 21:59:48 +0800

    Merge branch 'main' of https://github.com/Clone-Softwares/Ateneo-Forum into testing

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,16 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('dashboard');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-Route::get('/dashboard', [PostController::class, 'index'])->middleware(['auth'])->name('dashboard');
-
+Route::get('RequestPost', [PostController::class,'index'] )->name('post.fetch');
 
 Route::middleware('auth')->group(function () {
+    Route::post('CreatePost', [PostController::class, 'store'])->name('post.store');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -27,6 +25,3 @@ require __DIR__.'/auth.php';
 Route::get('/post', function (){
     return view('post');
 });
-
-Route::get('RequestPost', [PostController::class,'index'] )->name('post.fetch');
-Route::post('CreatePost', [PostController::class, 'store'])->name('post.store');
