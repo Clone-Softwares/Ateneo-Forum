7ff6a90602b196a638630229434cc73d7d409a75
Lol <133163245+Tantalost@users.noreply.github.com>
2025-09-25 18:15:54 +0800

    fix: fixed the guest and auth to be able to view and changed the routing to index for the dashboard

---
+++ laravel-ateneoForum/laravel-ateneoForum/resources/views/components/text-input.blade.php
@@ -0,0 +1,6 @@
+@props(['disabled' => false, 'placeholderContent' => 'nana', 'name' => 'name'])
+
+<input
+name={{ $name }}
+placeholder="{{ $placeholderContent }}"
+@disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 mb-8 mt-3 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }} required>
