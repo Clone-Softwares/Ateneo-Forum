7371eb0c0f81d25e2b6622fa419fe82db06e1c41
Lol <133163245+Tantalost@users.noreply.github.com>
2025-09-25 22:00:42 +0800

    feat: merging testing branch to main

---
+++ laravel-ateneoForum/laravel-ateneoForum/laravel-ateneoForum/resources/views/components/application-logo.blade.php
@@ -0,0 +1,18 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 120" {{ $attributes }}>
+  <defs>
+    <style type="text/css"><![CDATA[
+      .brand { font: 700 56px/1 "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; fill: #0b66c3; }
+      .bubble { fill: #0b66c3; }
+      .eye { fill: #ffffff; }
+    ]]></style>
+  </defs>
+
+  <!-- Left chat/speech-bubble icon -->
+  <g transform="translate(24,14)">
+    <path class="bubble" d="M0 26c0-11.046 8.954-20 20-20h44c11.046 0 20 8.954 20 20v12c0 11.046-8.954 20-20 20H62l-10 12v-12H20C8.954 58 0 49.046 0 38V26z"/>
+    <circle class="eye" cx="38" cy="34" r="4"/>
+  </g>
+
+  <!-- Wordmark -->
+  <text x="120" y="76" class="brand">AteneoForum</text>
+</svg>
---
+++ laravel-ateneoForum/laravel-ateneoForum/laravel-ateneoForum/resources/views/components/post-interaction.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-row  justify-between mt-5" >
+    <div class="flex flex-row gap-1">
+        <x-button><i class="fa-solid fa-up-long"></i></x-button>
+        <x-button><i class="fa-solid fa-down-long"></i></x-button>
+    </div>
+    <div>
+        <x-button>Comment</x-button>
+    </div>
+
+
+</div>
